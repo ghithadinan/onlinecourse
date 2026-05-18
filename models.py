f083a13e64e131c89e3347550549b757527524c5
Ghitha Dinan <ghithadinanhs@gmail.com>
2026-05-18 19:16:50 +0700

    add models.py

--- models.py
+++ models.py
@@ -1,10 +1,58 @@
 from django.db import models
 
 
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='lessons'
+    )
+    title = models.CharField(max_length=200)
+    order = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Instructor(models.Model):
+    full_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Learner(models.Model):
+    full_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Enrollment(models.Model):
+    learner = models.ForeignKey(
+        Learner,
+        on_delete=models.CASCADE
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.learner} - {self.course}"
+
+
 class Question(models.Model):
     course = models.ForeignKey(
-        'Course',
-        on_delete=models.CASCADE,   # wajib untuk ForeignKey
+        Course,
+        on_delete=models.CASCADE,
         related_name='questions'
     )
     question_text = models.CharField(max_length=500)
@@ -17,7 +65,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(
         Question,
-        on_delete=models.CASCADE,   # wajib untuk ForeignKey
+        on_delete=models.CASCADE,
         related_name='choices'
     )
     choice_text = models.CharField(max_length=200)
@@ -29,8 +77,8 @@ class Choice(models.Model):
 
 class Submission(models.Model):
     enrollment = models.ForeignKey(
-        'Enrollment',
-        on_delete=models.CASCADE    # wajib untuk ForeignKey
+        Enrollment,
+        on_delete=models.CASCADE
     )
     choices = models.ManyToManyField(Choice)
     submission_time = models.DateTimeField(auto_now_add=True)
